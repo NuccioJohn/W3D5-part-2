f776216e5611ed2bc386a847b8d149796f806427
NuccioJohn <nuccio.john@gmail.com>
2016-07-15 12:55:12 -0400

    Second Commit

---
+++ app/src/main/java/com/mac/training/twoactivities/MagicEvent.java
@@ -0,0 +1,10 @@
+package com.mac.training.twoactivities;
+
+/**
+ * Created by admin on 7/15/2016.
+ */
+public class MagicEvent {
+    public MagicEvent(){
+
+    }
+}
--- app/src/main/java/com/mac/training/twoactivities/MainActivity.java
+++ app/src/main/java/com/mac/training/twoactivities/MainActivity.java
@@ -3,18 +3,36 @@ package com.mac.training.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Main Tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
+    }
+
+    @Subscribe
+    public void doMagic(MagicEvent magicevent){
+        Log.d(TAG, "This is Magic");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
